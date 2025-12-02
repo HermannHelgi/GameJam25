91e8bd17e06898e5a2806068150a60860faaa672
Dadi Russel <dadirussle@gmail.com>
2025-12-02 15:03:34 +0000

    added buncha changes

--- feature/enemyAi/yuleladSoundmanager.gd
+++ feature/enemyAi/yuleladSoundmanager.gd
@@ -11,19 +11,19 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func play_walking_sound() -> void:
-	audioEmitter.stream = walkingSounds[randi_range(0,walkingSounds.size())]
+	audioEmitter.stream = walkingSounds[randi_range(0,walkingSounds.size() - 1)]
 	audioEmitter.play()
 	
 func play_destroying_sound() -> void:
-	audioEmitter.stream = destroyingSounds[randi_range(0, destroyingSounds.size())]
+	audioEmitter.stream = destroyingSounds[randi_range(0, destroyingSounds.size() - 1)]
 	audioEmitter.play()
 	
 func play_laughing_sound() -> void:
-	audioEmitter.stream = laughingSounds[randi_range(0, laughingSounds.size())]
+	audioEmitter.stream = laughingSounds[randi_range(0, laughingSounds.size() - 1)]
 	audioEmitter.play()
 	
 func play_loosingItem_sound() -> void:
-	audioEmitter.stream = loosingItemSound[randi_range(0, loosingItemSound.size())]
+	audioEmitter.stream = loosingItemSound[randi_range(0, loosingItemSound.size() - 1)]
 	audioEmitter.play()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
