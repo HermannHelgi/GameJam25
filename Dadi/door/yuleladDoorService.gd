b332455c0a2974dd18664ea996e416885d1fd348
Dadi Russel <dadirussle@gmail.com>
2025-12-03 19:39:45 +0000

    added cat fix

--- Dadi/door/yuleladDoorService.gd
+++ Dadi/door/yuleladDoorService.gd
@@ -1,23 +1,19 @@
 extends Area3D
 
-var enteringBody : Node3D = null
-var exitedBody : Node3D = null
-@onready var collider : CollisionShape3D = $Area3D/CollisionShape3D
 @export var door : Node3D
-var body : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
-
-func _on_body_entered(body):
+	#body_entered.connect(_on_body_entered)
+	#body_exited.connect(_on_body_exited)
+	pass
+	
+func _on_area_3d_body_entered(body):
 	print(" ENTER " ,body , body.name)
-	if !door.isOpen and body is YuleLad:
+	if not door.isOpen and body is CollisionShape3D:
 		door.open_door()
 
-func _on_body_exited(body):
-	
+func _on_area_3d_body_exited(body):
 	if body is YuleLad:
 		# Close only if there are no YuleLads left in the area
 		var has_yulelad := false
--- Julia/itemModels/prefabs/E_To_Interact/animals/cat.gd
+++ Julia/itemModels/prefabs/E_To_Interact/animals/cat.gd
@@ -8,19 +8,21 @@ class_name Cat
 var recentlyPetted : bool = false
 var recentlyPettedTimer : float = 0.0
 var playSoundTimer : float = 0.0
+var changePosition : bool = false
 @export var audioPlayer : AudioStreamPlayer3D
 @export var purrSounds : Array[AudioStreamWAV]
 
-var recently_petted: bool = false
-var recently_petted_timer: float = 0.0
-var meow_timer: float = 0.0
 
 var current_animation: String = ""
 
 func _ready() -> void:
-	current_animation = "Sit"
-	if animation:
-		animation.play("Sit")
+	var starting_animation = randf_range(0, 1)
+	if starting_animation == 0:		
+		if animation:
+			animation.play("Sit")
+	else:
+		if animation:
+			animation.play("Lay")
 
 func check_player_distance() -> float:
 	var distance = catPosition.distance_to(player.global_position)
@@ -28,6 +30,7 @@ func check_player_distance() -> float:
 	
 
 func changeAnimation() -> void:
+	changePosition = false
 	var currentAnimation = animation.current_animation
 	if (currentAnimation == "Sit") : 
 		animation.play("Lay")
@@ -39,16 +42,16 @@ func play_random_purr() -> void:
 	if purrSounds.is_empty():
 		return
 	if audioPlayer and not audioPlayer.playing:
-		var idx := randi_range(0, purrSounds.size() - 1)
-		audioPlayer.stream = purrSounds[idx]
+		audioPlayer.stream = purrSounds[randi_range(0 , purrSounds.size() -1)]
 		audioPlayer.play()
-	
+		playSoundTimer = 0.0
+			
 func pet() -> void:
 	if(recentlyPetted):
 		return
 	else:
-		play_random_purr()
 		recentlyPetted = true
+		play_random_purr()
 	
 func _process(delta: float) -> void:
 	if(recentlyPetted):
@@ -56,14 +59,14 @@ func _process(delta: float) -> void:
 		recentlyPettedTimer += delta
 		if (recentlyPettedTimer > 3):
 			recentlyPetted = false
+			changePosition = true
 			playSoundTimer = recentlyPettedTimer
-	elif(playSoundTimer < 10.0):
+	elif(playSoundTimer < 15.0):
 		playSoundTimer += delta
-		print(playSoundTimer)
-	elif(playSoundTimer > 10.0):
+	elif(playSoundTimer > 15.0):
 		play_random_purr()
 	var playerDistance = check_player_distance()
-	if(playerDistance > 5.0 ) and playSoundTimer > 10:
+	if(playerDistance > 10.0 ) and playSoundTimer > 15 and changePosition:
 		changeAnimation()
 		
 	
